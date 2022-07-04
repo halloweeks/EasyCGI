72a967568b8ea4eeb342eff131fd47055b77bb75
Hallo Weeks <96823158+halloweeks@users.noreply.github.com>
2022-07-04 11:40:32 +0530

    Add files via upload

--- index.cpp
+++ index.cpp
@@ -3,25 +3,25 @@
 
 int main() {
 	if (cgi.isset("post","submit")) {
-		cgi.put << "name: " << cgi.value("post", "name").String() << "";
-		cgi.put << "number: " << cgi.value("post", "number").LongLong() << "";
+		cgi.put << "name: " << cgi.value("post", "name").String() << "\r\n";
+		cgi.put << "number: " << cgi.value("post", "number").LongLong() << "\r\n";
 	}
 	
-	cgi.put << "<!DOCTYPE html>";
-	cgi.put << "<html lang=\"en\">";
-	cgi.put << "<head>";
-	cgi.put << "<meta charset=\"utf-8\">";
-	cgi.put << "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
-	cgi.put << "<title>CGI From handling</title>";
-	cgi.put << "</head>";
-	cgi.put << "<body>";
-	cgi.put << "<h4>My CGI Form</h4>";
-	cgi.put << "<form action=\"\" method=\"POST\">";
-	cgi.put << "<input type=\"text\" name=\"name\" placeholder=\"Enter name\" />";
-	cgi.put << "<input type=\"text\" name=\"number\" placeholder=\"Enter number\" />";
-	cgi.put << "<input type=\"submit\" name=\"submit\" value=\"Submit\"/>";
-	cgi.put << "</form>";
-	cgi.put << "</body>";
-	cgi.put << "</html>";
+	cgi.put << "<!DOCTYPE html>\r\n";
+	cgi.put << "<html lang=\"en\">\r\n";
+	cgi.put << "<head>\r\n";
+	cgi.put << "<meta charset=\"utf-8\">\r\n";
+	cgi.put << "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n";
+	cgi.put << "<title>CGI From handling</title>\r\n";
+	cgi.put << "</head>\r\n";
+	cgi.put << "<body>\r\n";
+	cgi.put << "<h4>My CGI Form</h4>\r\n";
+	cgi.put << "<form action=\"\" method=\"POST\">\r\n";
+	cgi.put << "<input type=\"text\" name=\"name\" placeholder=\"Enter name\" />\r\n";
+	cgi.put << "<input type=\"text\" name=\"number\" placeholder=\"Enter number\" />\r\n";
+	cgi.put << "<input type=\"submit\" name=\"submit\" value=\"Submit\"/>\r\n";
+	cgi.put << "</form>\r\n";
+	cgi.put << "</body>\r\n";
+	cgi.put << "</html>\r\n";
 	return 0;
 }
